72b8451df15b8c85415ddd427763b0b6fa752259
Erik-Mfa <erikmfa190230@gmail.com>
2022-12-27 21:41:16 -0300

    Calendário / Login

---
+++ lib/composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd77a192f09f59e79b59663d87cfe0ec8af93b94b',
+        'name' => 'zuno/calendario',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'zuno/calendario' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd77a192f09f59e79b59663d87cfe0ec8af93b94b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
