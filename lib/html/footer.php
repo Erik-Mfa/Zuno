f95fdccf97ba5c63999c7a8121c86633ceab2246
Erik-Mfa <erikmfa190230@gmail.com>
2023-02-16 03:10:17 -0300

    sass

--- lib/html/footer.php
+++ lib/html/footer.php
@@ -3,10 +3,10 @@
 </div>
 
 <div class="fixed-bottom">
-    <p class="text-center mb-0" style="font-size: 15px; background: rgb(159, 222, 158);">2023 © ZUNO</p>
+    <p class="text-center mb-0 rounded-4" style="font-size: 15px; background: rgb(159, 222, 158);">2023 © ZUNO</p>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="<?php echo DIRPAGE.'node_modules/bootstrap/dist/js/bootstrap.bundle.js'?>"></script>
 <script src="<?php echo DIRPAGE.'lib/js/FullCalendar/main.min.js'; ?>"></script>
 <script src="<?php echo DIRPAGE.'lib/js/javascript.js'; ?>"></script>
 </body>
--- lib/html/form.php
+++ lib/html/form.php
@@ -1,2 +1,2 @@
-<div class="container p-4">
+<div class="container p-4" style="margin-top: 10rem; margin-bottom: 10rem">
         <div class="container d-flex justify-content-center text-center col-6 border border-top-0 border-bottom-0">
